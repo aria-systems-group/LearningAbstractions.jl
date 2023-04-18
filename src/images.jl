30f2f1d39e08bdbea8cbd5cf6108dd9c9444bc7b
John Jackson <jmcjack@gmail.com>
2023-04-18 21:51:10 +0000

    Remove negative GPs - not necessary anymore due to PB capability

--- src/images.jl
+++ src/images.jl
@@ -22,15 +22,6 @@ function state_bounds(states_vec, gps; local_gps_flag=false, local_gps_data=noth
     # Setup for bounding refined states with local GP regression
     if local_gps_flag
         tree = create_data_tree(local_gps_data[1], metric) 
-        local_neg_gps = [nothing for _ in 1:length(gps)]
-    else
-        # Setup all the global GP stuff
-        neg_gps = []
-        for gp in gps
-            neg_gp = deepcopy(gp)
-            neg_gp.alpha *= -1
-            push!(neg_gps, neg_gp)
-        end
     end
 
     # Generate new posterior bounds
@@ -103,9 +94,9 @@ function state_bounds(states_vec, gps; local_gps_flag=false, local_gps_data=noth
         if local_gps_flag
             create_local_gps!(local_gps[tid], gps, all_state_means[idx], tree, gp_x, local_gps_data[2])
             @views theta_vec_train_sq_sub = [theta_vec_train_squared_all[i][local_gps[tid].sub_idxs] for i=1:odims]
-            image, σ_bounds = bound_image([state[:,1], state[:,end-1]], local_gps[tid].gps, local_neg_gps, theta_vec_train_sq_sub, theta_vec_all, image_prealloc, delta_input_flag=delta_input_flag, approximate_σ_flag=approximate_σ_flag) 
+            image, σ_bounds = bound_image([state[:,1], state[:,end-1]], local_gps[tid].gps, theta_vec_train_sq_sub, theta_vec_all, image_prealloc, delta_input_flag=delta_input_flag, approximate_σ_flag=approximate_σ_flag) 
         else
-            image, σ_bounds = bound_image([state[:,1], state[:,end-1]], post_gps, neg_gps, theta_vec_train_squared_all, theta_vec_all, image_prealloc, delta_input_flag=delta_input_flag, approximate_σ_flag=approximate_σ_flag)
+            image, σ_bounds = bound_image([state[:,1], state[:,end-1]], post_gps, theta_vec_train_squared_all, theta_vec_all, image_prealloc, delta_input_flag=delta_input_flag, approximate_σ_flag=approximate_σ_flag)
         end
         image_vec[idx] = extent_to_SA(image)
         σ_bounds_vec[idx] = σ_bounds
@@ -122,16 +113,15 @@ Generate overapproximations of posterior mean and covariance functions using one
 # Arguments
 - `extent` - Discrete state extent 
 - `gps` - Vector of GPs and associated metadata
-- `neg_gps` - Vector of GPs with -1*α vector
 - `delta_input_flag` - True uses `x` as the known component 
 """
-function bound_image(extent, gps, neg_gps, theta_vec_train_squared, theta_vec, image_prealloc; delta_input_flag=false, data_deps=nothing, known_component=nothing, σ_ubs=nothing, approximate_σ_flag=false)
+function bound_image(extent, gps, theta_vec_train_squared, theta_vec, image_prealloc; delta_input_flag=false, data_deps=nothing, known_component=nothing, σ_ubs=nothing, approximate_σ_flag=false)
     # TODO: mod keyword handling and document
     ndims = length(gps) 
     image_extent = Vector{Vector{Float64}}(undef, ndims)
     σ_bounds = zeros(ndims) 
     for i=1:ndims 
-        image_extent[i], σ_bounds[i] = bound_extent_dim(gps[i], neg_gps[i], extent[1], extent[2], theta_vec_train_squared[i], theta_vec[i], image_prealloc, approximate_σ_flag=approximate_σ_flag)
+        image_extent[i], σ_bounds[i] = bound_extent_dim(gps[i], extent[1], extent[2], theta_vec_train_squared[i], theta_vec[i], image_prealloc, approximate_σ_flag=approximate_σ_flag)
         if delta_input_flag
             image_extent[i][1] += extent[1][i]
             image_extent[i][2] += extent[2][i]
@@ -140,7 +130,7 @@ function bound_image(extent, gps, neg_gps, theta_vec_train_squared, theta_vec, i
     return image_extent, σ_bounds
 end
 
-function bound_extent_dim(gp, neg_gp, lbf, ubf, theta_vec_train_squared, theta_vec, image_prealloc; approximate_μ_flag=false, approximate_σ_flag=true)
+function bound_extent_dim(gp, lbf, ubf, theta_vec_train_squared, theta_vec, image_prealloc; approximate_μ_flag=false, approximate_σ_flag=true)
 
     if approximate_μ_flag
         μ_L_lb, μ_U_ub = PosteriorBounds.compute_μ_bounds_approx(gp, lbf, ubf) 
